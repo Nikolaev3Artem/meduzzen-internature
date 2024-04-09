8d654fa7d97ecd4278056a11113ec79af5da9310
Nikolaev3Artem <artnik349@gmail.com>
2024-04-09 22:11:41 +0300

    Added docker to the project

--- app/main.py
+++ app/main.py
@@ -1,12 +1,6 @@
-from typing import Union
-import uvicorn
 from fastapi import FastAPI
-import os
-from routers import healthcheck
+from routers.healthcheck import router as health_check_router
 
 app = FastAPI()
 
-app.include_router(healthcheck.router)
-
-if __name__ == "__main__":
-    uvicorn.run('main:app', host=os.getenv('API_HOST'), port=int(os.getenv('API_PORT')), log_level="info", reload=os.getenv("DEBUG", 'False').lower() in ('true', '1', 't'))
+app.include_router(health_check_router)
