340507e52ca0915c6623fd127afa6a9b89e48ba1
Nikolaev3Artem <artnik349@gmail.com>
2024-04-09 19:58:23 +0300

    Replaced router func to routers package and removed python-dotenv

--- app/main.py
+++ app/main.py
@@ -1,17 +1,12 @@
 from typing import Union
 import uvicorn
 from fastapi import FastAPI
-
-from dotenv import load_dotenv
 import os
-load_dotenv()
+from routers import healthcheck
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"status_code": 200, "detail": "ok", "result": "working"}
+app.include_router(healthcheck.router)
 
 if __name__ == "__main__":
     uvicorn.run('main:app', host=os.getenv('API_HOST'), port=int(os.getenv('API_PORT')), log_level="info", reload=os.getenv("DEBUG", 'False').lower() in ('true', '1', 't'))
