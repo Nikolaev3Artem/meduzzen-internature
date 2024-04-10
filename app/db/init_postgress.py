564e99079c0dc78f007b0ed38c4a45f78961f8ec
Nikolaev3Artem <artnik349@gmail.com>
2024-04-10 22:03:47 +0300

    Added redis to the project

--- app/db/init_postgress.py
+++ app/db/init_postgress.py
@@ -1,4 +1,3 @@
-import os
 from core.config import settings
 
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -6,18 +5,17 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = f'postgresql+asyncpg://{settings.postgres_user}:{settings.postgres_password}@{settings.postgres_host}/{settings.postgres_db}'
-
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(settings.postgres_database_url, echo=True)
 Base = declarative_base()
 
-async_session = sessionmaker(
-    engine, class_=AsyncSession, expire_on_commit=False
-)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
 async def init_models():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-        
+
+
 async def get_session() -> AsyncSession:
     async with async_session() as session:
         yield session
--- app/routers/healthcheck.py
+++ app/routers/healthcheck.py
@@ -3,11 +3,15 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 
-from db.init_db import get_session
+from db.init_postgress import get_session
+from db.init_redis import r
 
 router = APIRouter()
+
 
 @router.get("/")
 async def healthcheck(session: AsyncSession = Depends(get_session)):
     await session.execute(select(1))
+    await r.set("test", "redis connected")
+    print(await r.get("test"))
     return {"status_code": 200, "detail": "ok", "result": "working"}
