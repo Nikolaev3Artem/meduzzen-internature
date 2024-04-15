0c3745c6656bd2877ef7bc0cfeacfc8d937f939e
Nikolaev3Artem <artnik349@gmail.com>
2024-04-15 11:48:32 +0300

    Remade id using uuid type

--- app/db/alchemy/alembic/versions/000001_created_users.py
+++ app/db/alchemy/alembic/versions/000001_created_users.py
@@ -5,10 +5,12 @@ Revises:
 Create Date: 2024-04-14 21:44:03.433504
 
 """
+import uuid
 from typing import Sequence, Union
 
 import sqlalchemy as sa
 from alembic import op
+from sqlalchemy.dialects.postgresql import UUID
 
 # revision identifiers, used by Alembic.
 revision: str = "000001"
@@ -20,7 +22,7 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     op.create_table(
         "users",
-        sa.Column("uuid", sa.Integer, primary_key=True),
+        sa.Column("id", UUID(as_uuid=True), primary_key=True, default=uuid.uuid4),
         sa.Column("email", sa.String(80), unique=True),
         sa.Column("password", sa.String),
         sa.Column("username", sa.String(100)),
--- app/db/alchemy/models.py
+++ app/db/alchemy/models.py
@@ -1,12 +1,17 @@
+import uuid
+
 from db.postgress import Base
-from sqlalchemy import Integer, String
+from sqlalchemy import String
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
 
 
 class User(Base):
     __tablename__ = "users"
 
-    uuid: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    email: Mapped[str] = mapped_column(String(80), unique=True, index=True)
+    id: Mapped[int] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
+    email: Mapped[str] = mapped_column(String(80), unique=True)
     password: Mapped[str] = mapped_column(String)
     username: Mapped[str] = mapped_column(String)
