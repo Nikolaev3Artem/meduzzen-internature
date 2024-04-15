20515bcf49fdc434eeef63d4204e6a13a1ebde31
Nikolaev3Artem <artnik349@gmail.com>
2024-04-15 17:58:24 +0300

    Updated models.py

--- app/db/alchemy/models.py
+++ app/db/alchemy/models.py
@@ -1,12 +1,17 @@
 import uuid
 
-from db.postgress import Base
 from sqlalchemy import String
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
 
 
 class IDBase(Base):
+    __abstract__ = True
+
     id: Mapped[int] = mapped_column(
         UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
     )
@@ -17,4 +22,4 @@ class User(IDBase):
 
     email: Mapped[str] = mapped_column(String, unique=True)
     password: Mapped[str] = mapped_column(String)
-    username: Mapped[str] = mapped_column(String)
+    username: Mapped[str] = mapped_column(String(100))
